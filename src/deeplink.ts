684a325e646c2dd4d5314b18f34584f3a0d82b11
johnnyreilly <johnny_reilly@hotmail.com>
2022-12-04 08:12:45 +0000

    fix: prettier

--- src/deeplink.ts
+++ src/deeplink.ts
@@ -13,41 +13,41 @@ export async function deeplink(loginUrl: string) {
 		throw new Error("loginUrl is required");
 	}
 
-    const pathAndQuery = location.pathname + location.search;
-    console.log(`deeplink: URL before: ${pathAndQuery}`);
+	const pathAndQuery = location.pathname + location.search;
+	console.log(`deeplink: URL before: ${pathAndQuery}`);
 
-    const deeplinkPathAndQuery = localStorage.getItem(deeplinkPathAndQueryKey);
+	const deeplinkPathAndQuery = localStorage.getItem(deeplinkPathAndQueryKey);
 
-    const isAuth = await isAuthenticated();
+	const isAuth = await isAuthenticated();
 
-    if (isAuth) {
-        if (deeplinkPathAndQuery && pathAndQuery === "/") {
-            console.log(`deeplink: Redirecting to ${deeplinkPathAndQuery}`);
-            localStorage.removeItem(deeplinkPathAndQueryKey);
-            history.replaceState(null, "", deeplinkPathAndQuery);
-        }
-    } else if (!deeplinkPathAndQuery) {
-        if (pathAndQuery !== "/" && pathAndQuery !== loginUrl) {
-            console.log(
-                `deeplink: Storing redirect URL of ${pathAndQuery} and redirecting to ${loginUrl}`
-            );
-            localStorage.setItem(deeplinkPathAndQueryKey, pathAndQuery);
-            location.href = loginUrl;
-        } else {
-            console.log(`deeplink: Redirecting to ${loginUrl}`);
-            location.href = loginUrl;
-        }
-    }
+	if (isAuth) {
+		if (deeplinkPathAndQuery && pathAndQuery === "/") {
+			console.log(`deeplink: Redirecting to ${deeplinkPathAndQuery}`);
+			localStorage.removeItem(deeplinkPathAndQueryKey);
+			history.replaceState(null, "", deeplinkPathAndQuery);
+		}
+	} else if (!deeplinkPathAndQuery) {
+		if (pathAndQuery !== "/" && pathAndQuery !== loginUrl) {
+			console.log(
+				`deeplink: Storing redirect URL of ${pathAndQuery} and redirecting to ${loginUrl}`
+			);
+			localStorage.setItem(deeplinkPathAndQueryKey, pathAndQuery);
+			location.href = loginUrl;
+		} else {
+			console.log(`deeplink: Redirecting to ${loginUrl}`);
+			location.href = loginUrl;
+		}
+	}
 }
 
 async function isAuthenticated() {
-    try {
-        const response = await fetch("/.auth/me");
+	try {
+		const response = await fetch("/.auth/me");
 		const authMe = (await response.json()) as AuthMe;
-        const isAuth = authMe.clientPrincipal !== null;
-        return isAuth;
-    } catch (error) {
-        console.error("Failed to fetch /.auth/me", error);
-        return false;
-    }
+		const isAuth = authMe.clientPrincipal !== null;
+		return isAuth;
+	} catch (error) {
+		console.error("Failed to fetch /.auth/me", error);
+		return false;
+	}
 }
